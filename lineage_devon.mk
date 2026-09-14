e919c639cf2377a9db1ed7b7a57fe3a50d8065ab
frosti500 <ajayx265@gmail.com>
2026-09-14 21:52:34 +0530

    Update lineage_devon.mk

--- lineage_devon.mk
+++ lineage_devon.mk
@@ -13,6 +13,15 @@ $(call inherit-product, device/motorola/devon/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device config
+WITH_GMS := false
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+KERNEL_VERSION := 4.19
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_devon
 PRODUCT_DEVICE := devon
@@ -26,3 +35,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="devon_g-user 11 T2SNS33.73-22-3-19 565799 release-keys" \
     BuildFingerprint=motorola/devon_g/devon:11/T2SNS33.73-22-3-19/565799:user/release-keys \
     DeviceProduct=devon_g
+    RisingChipset="Snapdragon 680" \
+    RisingMaintainer="hades"
